041269cb5037d64c7647fc90206c2f0e95cc5998
JR <jr.poinsot@gmail.com>
2020-02-12 23:00:05 +0100

    Migrate angular 9

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -11,10 +11,12 @@ export class HomeComponent implements OnInit {
 
   constructor(private userService: UserService) { }
 
-  users$: Observable<any>;
+  users: any;
 
   ngOnInit() {
-    this.users$ = this.userService.getUsers();
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+    })
   }
 
   getUsers(): void {
@@ -22,7 +24,9 @@ export class HomeComponent implements OnInit {
   }
 
   getUser(userId: string): void {
-    this.users$ = this.userService.getUser(userId);
+    this.userService.getUser(userId).subscribe(user => {
+      this.users = user;
+    })
   }
 
 }
